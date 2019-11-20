03d580107920717d13803b1d32765d332371ebe6
Vivekanand P V <vivekanandpv@outlook.com>
2019-11-20 23:21:46 +0530

    finished

--- src/app/topic2/topic2.component.ts
+++ src/app/topic2/topic2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-topic2',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topic2.component.css']
 })
 export class Topic2Component implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  navigationHandler() {
+    this.router.navigate(['/topic1', '2018', 'Mumbai'], {
+      queryParams: {
+        from: 'India',
+        limit: 50
+      },
+      fragment: 'FragmentIsHere'
+    });
   }
-
 }
