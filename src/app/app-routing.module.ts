085429c79bd83bb29ac13ed67f4b3696c549fbd0
Vivekanand P V <vivekanandpv@outlook.com>
2019-11-20 22:45:50 +0530

    finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Topic1Component } from './topic1/topic1.component';
+import { Topic2Component } from './topic2/topic2.component';
+import { Topic3Component } from './topic3/topic3.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  {
+    path: 'topic1',
+    component: Topic1Component
+  },
+  {
+    path: 'topic2',
+    component: Topic2Component
+  },
+  {
+    path: 'topic3',
+    component: Topic3Component
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
